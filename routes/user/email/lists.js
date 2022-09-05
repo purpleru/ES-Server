7c96ae7ea1dd9babeb816c9f0bc656b1cfaf3613
gudu <3072953029@qq.com>
2022-09-05 16:33:16 +0800

    完成了邮箱服务列表分页功能

--- routes/user/email/lists.js
+++ routes/user/email/lists.js
@@ -1,15 +1,50 @@
 const { Email, EmailJoiSchema } = require('../../../mongo/models/Email');
-module.exports = function (req, res) {
-    Email.find({}, '-author.password').populate('user', '-password').exec(function (err, docs) {
-        if (err) {
-            return res.json({
-                code: 500,
-                msg: err.message
-            });
-        }
-        res.json({
-            code: 200,
-            lists: docs
+const Joi = require('joi');
+const validateQueryJoi = Joi.object({
+    currentPage: Joi.number().required().min(1).messages({
+        'any.required': '缺少当前页码数参数'
+    }),
+    pageSize: Joi.number().default(5).min(5).max(50)
+});
+module.exports = async function (req, res, next) {
+
+    let { currentPage, pageSize } = req.query;
+
+    const { error, value } = validateQueryJoi.validate({
+        currentPage,
+        pageSize
+    });
+
+    if (error) {
+        return next({
+            msg: error.message,
+            code: 400
         });
+    }
+
+    currentPage = value.currentPage;
+    pageSize = value.pageSize;
+
+    try {
+        var count = await Email.countDocuments({});
+
+        var total = Math.ceil(count / pageSize);
+
+        currentPage = currentPage > total ? total : currentPage;
+
+        var start = (currentPage - 1) * pageSize;
+
+        var emailLists = await Email.find({}, '-author.password').populate('user', '-password').skip(start).limit(pageSize);
+
+    } catch (err) {
+        return next(err);
+    }
+
+    res.json({
+        code: 200,
+        lists: emailLists,
+        total,
+        currentPage,
+        pageSize
     });
 }
