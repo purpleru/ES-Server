6af6c72b1f7fd6ef60b7f68bfa2e07d6865ff941
gudu <3072953029@qq.com>
2022-09-16 16:27:38 +0800

    允许浏览器客户端向commit接口发起跨域请求

--- routes/other/index.js
+++ routes/other/index.js
@@ -2,7 +2,15 @@ const express = require('express');
 
 const other = express.Router();
 
-other.post('/commit/:key', require('./commit'));
+other.post('/commit/:key', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    if (req.method === 'OPTIONS') {
+        res.end();
+    } else {
+        next();
+    }
+}, require('./commit'));
 other.post('/validate', require('./validateService'));
 
 module.exports = other;
